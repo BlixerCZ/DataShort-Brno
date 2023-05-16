b33782ef4cbd24f65f3624885129caff62862d34
BlixerCZ <98454376+BlixerCZ@users.noreply.github.com>
2023-05-16 14:51:14 +0200

    Add files via upload

---
+++ brnoid.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BrnoID</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="manifest" href="manifest-brnoid.json">
+</head>
+
+<body>
+    <iframe src="https://www.brnoid.cz/cs/" frameborder="0" style="height: calc(100vh - 4px); width: 100vw"></iframe>
+</body>
+</html>
